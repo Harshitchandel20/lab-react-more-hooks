882698f4144934443dfd14961f462fb90864a2ef
Harshitchandel20 <harsitchandel100@.gmail.com>
2024-04-24 01:08:10 +0530

    done

---
+++ vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import TaskList from './Components/Task'
+
+
+function App() {
+
+  return (
+    <>
+    <TaskList />
+    </>
+  )
+}
+
+export default App
---
+++ vite-project/src/Components/Task.jsx
@@ -0,0 +1,74 @@
+import { useRef } from 'react'
+import { useReducer } from 'react'
+
+const reducer = (initState,action)=>{
+    switch(action.type){
+        case "ADD_TASK" :{
+            return [...initState,{id: Date.now(), text:action.payload,isVisible:true}]
+        }
+        case "TOGGLE_TASK":{
+            return initState.map((ele)=>{
+               return ele.id==action.payload ? {...ele,isVisible: !ele.isVisible} :ele;
+            })
+        }
+
+        default :
+        return initState
+    }
+
+}
+
+const TaskList = () => {
+
+        const inputRef = useRef(null);
+        const [state,dispatch] = useReducer(reducer, []);
+
+        const addTask = (e)=>{
+            dispatch({type:"ADD_TASK",payload:e.target.value})
+        }
+
+        const toggleTask = (taskId)=>{
+            dispatch({type:"TOGGLE_TASK",payload:taskId})
+        }
+
+        const scrollTop = ()=>{
+            window.scrollTo({top:0,behavior:"smooth"}),
+            inputRef.current.focus()
+        }
+    return (
+        <>
+      <div>
+        <input type="text" ref={inputRef} onKeyDown={(e)=>{
+            if(e.key=="Enter"){
+            addTask(e)}
+        }
+        }/>
+
+        <div>{
+            state.map((ele)=>{
+                return ( <li key={ele.id}>
+                    
+                        {ele.isVisible ? <div>
+                            {ele.text}
+                            <button onClick={()=>toggleTask(ele.id)} >Toggle</button>
+                        </div>: <div>
+                            The content is hidden
+                            <button onClick={()=>toggleTask(ele.id)} >Toggle</button>
+                        </div>}
+                        
+                    
+
+                    </li>
+                    )
+            })
+        }
+        </div>
+        
+      </div>
+      <button onClick={()=> scrollTop()}>Scroll to top</button>
+      </>
+    )
+  
+}
+
+export default TaskList
